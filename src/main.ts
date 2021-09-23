30dcfb6e841297eca5470eb517760ca04c620a1c
fuqi1001 <fuqiqi12345@gmail.com>
2021-09-23 15:51:15 -0400

    x

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,19 @@ async function run(): Promise<void> {
   try {
     const gitRef: string = core.getInput('refs')
     const arr: string[] = gitRef.split('/')
-    const branchName: string = arr[arr.length - 1]
-    core.setOutput('tag', branchName)
+    const refBranchName: string = arr[arr.length - 1]
+
+    const baseRef: string = core.getInput('base_ref')
+    const baseRefArr: string[] = baseRef.split('/')
+    const baseName: string = baseRefArr[baseRefArr.length - 1]
+
+    const headRef: string = core.getInput('head_ref')
+    const headRefArr: string[] = headRef.split('/')
+    const headName: string = headRefArr[headRefArr.length - 1]
+
+    core.setOutput('tag', refBranchName)
+    core.setOutput('base', baseName)
+    core.setOutput('head', headName)
   } catch (error) {
     core.setFailed(error.message)
   }
