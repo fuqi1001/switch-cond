56037d2e4b8a6ec62a18885976b3550663011919
fuqi1001 <fuqiqi12345@gmail.com>
2021-10-22 10:47:13 -0400

    main set

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
+    const mainSet = new Set()
+    mainSet.add('main')
+    mainSet.add('master')
+    mainSet.add('dev')
+
     const gitRef: string = core.getInput('refs')
     const arr: string[] = gitRef.split('/')
     const refBranchName: string = arr[arr.length - 1]
@@ -11,7 +16,7 @@ async function run(): Promise<void> {
 
     const isMainVar: string = core.getInput('isMain')
     const isMain: boolean = isMainVar === 'true'
-    const useMainCommon: boolean = tag === 'main' || tag === 'master'
+    const useMainCommon: boolean = mainSet.has(tag)
     const commonBranch: string = isMain || useMainCommon ? 'master' : tag
 
     core.setOutput('tag', tag)
